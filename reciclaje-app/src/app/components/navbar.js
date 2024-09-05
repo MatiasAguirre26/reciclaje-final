d965815482501f1e8f5a025076e8cbca05b6ecf6
Matias Aguirre <matiasezeaguirre@gmail.com>
2024-09-05 00:46:51 -0300

    estilando los iconos de la barra de navegacion

--- reciclaje-app/src/app/components/navbar.js
+++ reciclaje-app/src/app/components/navbar.js
@@ -3,19 +3,26 @@ import styles from '../styles/navbar.module.css';
 
 // Importamos los iconos SVG
 import HomeIcon from '/public/icons/home.svg';
+import MapIcon from '/public/icons/map.svg';
+import RecycleIcon from '/public/icons/recycle.svg';
+import CoinsIcon from '/public/icons/coins.svg';
 
 export default function Navbar() {
     return (
       <nav className={styles.nav}>
         <ul className={styles.navList}>
           <li className={styles.navItem}>
-            <a href="/dashboard"><HomeIcon className={styles.navIcon} />
-              Inicio
-            </a>
+            <a href="/dashboard" className={styles.navLink}><HomeIcon className={styles.navIcon} /></a>
           </li>
-          <li className={styles.navItem}><a href="/exchange">Canje</a></li>
-          <li className={styles.navItem}><a href="/map">Mapa</a></li>
-          <li className={styles.navItem}><a href="/recycle">Reciclar</a></li>
+          <li className={styles.navItem}>
+            <a href="/exchange" className={styles.navLink}><CoinsIcon className={styles.navIcon} /></a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="/map" className={styles.navLink}><MapIcon className={styles.navIcon} /></a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="/recycle" className={styles.navLink}><RecycleIcon className={styles.navIcon} /></a>
+          </li>
         </ul>
       </nav>
     );
