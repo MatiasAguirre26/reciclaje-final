49d96af974eed8979e959275ae5d728aac4c33e8
Matias Aguirre <matiasezeaguirre@gmail.com>
2024-09-19 22:28:44 -0300

    maquetando la pagina de rewards y haciendo un array con las categorias y recompensas con un ID unico para redirigir a la pagina de detalles

--- src/app/rewards/[id]/page.js
+++ src/app/rewards/[id]/page.js
@@ -1,5 +1,20 @@
-'use client'
+'use client';
 
-export default function DetailReward() {
-    return <h1>¡Hola, Soy Detalles de la recompensa!</h1>
+import { useRouter } from 'next/navigation';
+
+export default function RewardDetails({ params }) {
+  const router = useRouter();
+  const { id } = params;
+
+  // Puedes hacer fetch de la recompensa con el id
+  // O también pasarle los detalles mediante props al hacer el routing
+  return (
+    <div className="text-white">
+      <h1>Detalles de la Recompensa {id}</h1>
+      {/* Muestra más información de la recompensa aquí */}
+      <button onClick={() => router.back()} className="p-2 mt-4 bg-blue-500 rounded">
+        Volver
+      </button>
+    </div>
+  );
 }
--- src/app/rewards/page.js
+++ src/app/rewards/page.js
@@ -2,13 +2,23 @@
 
 import Navbar from '@/components/navbar';
 import SearchBar  from "@/components/search-bar";
+import Link from 'next/link';
+import { buttonVariants } from "@/components/ui/button"; // Tu botón personalizado
 import styles from '@/styles/rewards-page.module.css';
 import CoinsIcon from '/public/icons/coins.svg';
-
+import TicketIcon from '/public/icons/ticket.svg';
 
 export default function rewards() {
     const categories = ['Todos', 'Coto', 'Día', 'Carrefour'];
-    const rewards = Array(6).fill({ discount: '15%', category: 'Alimentos' });
+    // const rewards = Array(6).fill({ discount: '15%', category: 'Alimentos' });
+  // Aquí cada recompensa debería tener un ID único para poder identificarla
+    const rewards = [
+      { id: 1, discount: '15%', category: 'Alimentos' },
+      { id: 2, discount: '10%', category: 'Ropa' },
+      { id: 3, discount: '20%', category: 'Electrónica' }
+    ];
+
+
     return (
     <div className="mx-[30px] grid gap-[20px] justify-center my-8 text-white">
         <div className="mb-8 text-3xl text-center">
@@ -33,13 +43,16 @@ export default function rewards() {
         </div>
         <div className={styles.rewardsList}>
           {rewards.map((reward, index) => (
-            <div key={index} className={styles.rewardCard}>
-              <div className={styles.rewardImage}>🎁</div>
-              <p>{reward.discount}</p>
-              <p>{reward.category}</p>
-            </div>
+            <Link href={`/rewards/${index}`} key={index} className={styles.rewardCardLink}>
+              <div className={styles.rewardCard}>
+                <div className={styles.rewardImage}><TicketIcon/></div>
+                <p>{reward.discount}</p>
+                <p>{reward.category}</p>
+              </div>
+            </Link>
           ))}
         </div>
+
         <Navbar />
     </div>
     )
