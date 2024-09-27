244837fe170be92d52f37245438829c6fc2320c2
rociobp <rociobarriospaez@gmail.com>
2024-09-27 13:15:18 -0300

    cambios en rewards

--- src/app/rewards/[id]/page.js
+++ src/app/rewards/[id]/page.js
@@ -1,44 +1,41 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import CoinsIcon from '/public/icons/coins.svg';
 import useRewardStore from '@/app/stores/useRewardStore';
 import Link from 'next/link';
 import { buttonVariants } from "@/components/ui/button";
 import TicketIcon from '/public/icons/ticket.svg';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useSession } from "next-auth/react";
 
+export default function RewardDetails({ params }) {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const userId = session?.user?.userId;
 
-export default function RewardDetails() {
-  const { data: session } = useSession(); // Obtiene la sesión
-  const userId = session.user.userId; // Asegúrate de que esta propiedad exista
+  const { selectedReward, redeemReward, userPoints, setSelectedReward } = useRewardStore();
+  const [redeemed, setRedeemed] = useState(false);
 
-  const { selectedReward, redeemReward, userPoints } = useRewardStore();
-  const [redeemed, setRedeemed] = useState(false); // Para mostrar un mensaje de confirmación
+  useEffect(() => {
+    if (!selectedReward) {
+      // Si no hay recompensa seleccionada, redirigir a la página de recompensas
+      router.push('/rewards');
+    }
+  }, [selectedReward, router]);
 
   if (!selectedReward) {
-    return <p>No hay detalles de la recompensa disponibles</p>;
+    return null; // O puedes mostrar un loader aquí
   }
-
-  // const handleRedeem = () => {
-  //   if (userPoints >= selectedReward.cost) {
-  //     redeemReward(selectedReward.cost);  // Descuenta los puntos
-  //     setRedeemed(true);  // Muestra el mensaje de confirmación
-  //     console.log("Puntos después del canje:", useRewardStore.getState().userPoints); // Verifica que los puntos se han actualizado
-  //   } else {
-  //     setRedeemed(false);  // En caso de no tener suficientes puntos
-  //   }
-  // };
-
 
   const handleRedeem = async () => {
     if (userPoints >= selectedReward.cost) {
-      // Lógica para canjear los puntos
-      redeemReward(selectedReward.cost);  // Descuenta los puntos
-      setRedeemed(true);  // Muestra el mensaje de confirmación
+      redeemReward(selectedReward.cost);
+      setRedeemed(true);
       
-      const couponValue = selectedReward.cost; // valor del cupón
-      const couponValidity = selectedReward.expiration; // vigencia del cupón
+      const couponValue = selectedReward.cost;
+      const couponValidity = selectedReward.expiration;
 
       console.log("Datos a enviar:", {
         userId,
@@ -47,32 +44,33 @@ export default function RewardDetails() {
         couponValidity,
       });
   
-      // Realiza el fetch
-      const response = await fetch('/api/redeem', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          userId,
-          totalPoints: userPoints,
-          couponValue,
-          couponValidity,
-        }),
-      });
+      try {
+        const response = await fetch('/api/redeem', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            userId,
+            totalPoints: userPoints,
+            couponValue,
+            couponValidity,
+          }),
+        });
   
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Canje exitoso:', data);
-      } else {
-        console.error('Error en el canje:', response.statusText);
+        if (response.ok) {
+          const data = await response.json();
+          console.log('Canje exitoso:', data);
+        } else {
+          console.error('Error en el canje:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error al realizar el canje:', error);
       }
-  
     } else {
-      setRedeemed(false);  // En caso de no tener suficientes puntos
+      setRedeemed(false);
     }
   };
-  
 
   return (
     <div className="grid gap-6">
@@ -85,27 +83,22 @@ export default function RewardDetails() {
         </div>
       </div>
       <div className="max-w-3xl p-6 mx-auto my-12 text-center text-white bg-gray-900 rounded-lg">
-      <div className="flex justify-center mb-4">
-        <TicketIcon className="w-12 h-12 text-[--color-primary]"/>
-      </div>
-      <p className="text-xl font-bold">{selectedReward.discount}</p>
-      <p className="text-sm">{selectedReward.category}</p>
-      <p className="text-lg">Valor del cupon: <span className="font-bold">{selectedReward.cost} Puntos</span></p>
-        {/* <p>Descuento: {selectedReward.discount}</p>
-        <p>Categoría: {selectedReward.category}</p>
-        <p>Detalles: {selectedReward.details}</p> */}
+        <div className="flex justify-center mb-4">
+          <TicketIcon className="w-12 h-12 text-[--color-primary]"/>
+        </div>
+        <p className="text-xl font-bold">{selectedReward.discount}</p>
+        <p className="text-sm">{selectedReward.category}</p>
+        <p className="text-lg">Valor del cupón: <span className="font-bold">{selectedReward.cost} Puntos</span></p>
         <p>Vencimiento: {selectedReward.expiration}</p>
       </div>
-      {/* Mensaje de confirmación al canjear */}
       {redeemed && (
         <p className="text-center text-green-500">¡Canje exitoso! Has canjeado la recompensa.</p>
       )}
 
       <div className='flex justify-center gap-4'>
         <Link href="/rewards" className={buttonVariants({ variant: "secondary", size: "lg", className: "font-bold" })}>Volver</Link>
-        <Link href="/dashboard" onClick={handleRedeem} className={buttonVariants({ variant: "default", size: "lg", className: "font-bold" })}>Canjear</Link>
+        <button onClick={handleRedeem} className={buttonVariants({ variant: "default", size: "lg", className: "font-bold" })}>Canjear</button>
       </div>      
     </div>
-
   );
 }
