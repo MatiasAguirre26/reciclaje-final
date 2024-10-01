2f772f66ed9ea4b3b50a810bde734312728e8b66
Brenda <brendaruthherrera@gmail.com>
2024-09-30 23:02:35 -0300

    fix error in route.js

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]/options';
+
 
 export async function POST(req) {
   try {
